da2f2363157dcab694cd6f6533412d1f371fd44d
tpoupompo <mitsuki.ikeda232@gmail.com>
2026-04-25 17:12:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,42 @@
 var cacheName = 'you-should-study-v1.0.9';
-var filesToCache = [
-  './index.html',
-];
+var filesToCache = ['./index.html'];
 
-/* サービスワーカー起動して、コンテンツをキャッシュする */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+    // 新しいSWをすぐに有効化
+    self.skipWaiting();
 });
 
-/* オフライン時はキャッシュからコンテンツを取得する */
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                // 古いキャッシュを全部削除
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    // 開いているページをすぐに新しいSWで制御
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        // キャッシュより常にネットワークを優先し、失敗時にキャッシュを使う
+        fetch(e.request).then(function(response) {
+            const clone = response.clone();
+            caches.open(cacheName).then(function(cache) {
+                cache.put(e.request, clone);
+            });
+            return response;
+        }).catch(function() {
+            return caches.match(e.request);
+        })
+    );
 });
